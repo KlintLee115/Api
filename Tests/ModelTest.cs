a14e7d767aa440863e10f06a98baacd4a6d5bebc
KlintLee115 <klintlee1@gmail.com>
2024-11-03 16:41:26 -0700

    worked on more tests

--- Tests/ModelTest.cs
+++ Tests/ModelTest.cs
@@ -1,165 +1,206 @@
-﻿// using Api.Database;
-// using Api.Model;
-// using Bogus;
-// using Microsoft.EntityFrameworkCore;
-// using Xunit;
+﻿using Api.Database;
+using Api.Model;
+using Api.Model.People;
+using Bogus;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
 
-// namespace Api.Tests;
+namespace Api.Tests;
 
-// public class ModelTest()
-// {
-//     private static DbContextOptions<AppDbContext> GetDbContextOptions()
-//     {
-//         return new DbContextOptionsBuilder<AppDbContext>()
-//             .UseNpgsql("Host=localhost;Port=5432;Username=postgres;Password=root;Database=mydatabase")
-//             .Options;
-//     }
+public class ModelTest()
+{
+    private static DbContextOptions<AppDbContext> GetDbContextOptions()
+    {
+        return new DbContextOptionsBuilder<AppDbContext>()
+            .UseNpgsql("Host=localhost;Port=5432;Username=postgres;Password=root;Database=mydatabase")
+            .Options;
+    }
 
-//     private static async Task TruncateTablesAsync(AppDbContext context)
-//     {
-//         // You can add more tables as needed
-//         await context.Database.ExecuteSqlRawAsync("TRUNCATE TABLE public.\"FamilyMembers\" CASCADE");
-//         await context.Database.ExecuteSqlRawAsync("TRUNCATE TABLE public.\"Families\" CASCADE");
-//         await context.Database.ExecuteSqlRawAsync("TRUNCATE TABLE public.\"AthleteInfo\" CASCADE");
-//         await context.Database.ExecuteSqlRawAsync("TRUNCATE TABLE public.\"Admins\" CASCADE");
-//         await context.Database.ExecuteSqlRawAsync("TRUNCATE TABLE public.\"Courses\" CASCADE");
-//         await context.Database.ExecuteSqlRawAsync("TRUNCATE TABLE public.\"Facilities\" CASCADE");
-//         await context.Database.ExecuteSqlRawAsync("TRUNCATE TABLE public.\"Coaches\" CASCADE");
-//         await context.Database.ExecuteSqlRawAsync("TRUNCATE TABLE public.\"Customers\" CASCADE");
-//         await context.Database.ExecuteSqlRawAsync("TRUNCATE TABLE public.\"Accounts\" CASCADE");
-//         // Add any other tables that need truncation here
-//     }
+    private static void TruncateTables(AppDbContext context)
+    {
+        context.Database.ExecuteSqlRaw("TRUNCATE TABLE public.\"Families\" CASCADE");
+        context.Database.ExecuteSqlRaw("TRUNCATE TABLE public.\"AthleteInfo\" CASCADE");
+        context.Database.ExecuteSqlRaw("TRUNCATE TABLE public.\"CustomerFinancialInfo\" CASCADE");
+        context.Database.ExecuteSqlRaw("TRUNCATE TABLE public.\"FinancialInfo\" CASCADE");
+        context.Database.ExecuteSqlRaw("TRUNCATE TABLE public.\"CourseSchedules\" CASCADE");
+        context.Database.ExecuteSqlRaw("TRUNCATE TABLE public.\"Admins\" CASCADE");
+        context.Database.ExecuteSqlRaw("TRUNCATE TABLE public.\"Courses\" CASCADE");
+        context.Database.ExecuteSqlRaw("TRUNCATE TABLE public.\"Facilities\" CASCADE");
+        context.Database.ExecuteSqlRaw("TRUNCATE TABLE public.\"Coaches\" CASCADE");
+        context.Database.ExecuteSqlRaw("TRUNCATE TABLE public.\"Customers\" CASCADE");
+        context.Database.ExecuteSqlRaw("TRUNCATE TABLE public.\"IndividualAccounts\" CASCADE");
+    }
 
-//     private static List<Customer> GenerateCustomers(int count)
-//     {
-//         return new Faker<Customer>()
-//             .CustomInstantiator(f =>
-//                 new Customer(
-//                     f.Person.FullName,
-//                     f.Internet.Email(),
-//                     f.Internet.Password(),
-//                     f.Phone.PhoneNumber("##########")
-//                 )).Generate(count);
-//     }
+    private static List<Customer> GenerateCustomers(int count, List<Family>? familiesForRandPick, List<Customer.RolesEnum>? rolesForRandPick)
+    {
 
-//     private static async Task<List<Family>> CreateFamilies(AppDbContext context, int amt)
-//     {
-//         List<Family> families = [];
+        if (familiesForRandPick != null && rolesForRandPick != null)
+        {
+            return new Faker<Customer>()
+            .CustomInstantiator(f =>
+                new Customer(
+                    f.Person.FullName,
+                    f.Internet.Email(),
+                    f.Internet.Password(),
+                    f.Phone.PhoneNumber("##########"),
+                    familiesForRandPick[new Random().Next(familiesForRandPick.Count)].Id,
+                    rolesForRandPick[new Random().Next(rolesForRandPick.Count)]
+                )).Generate(count);
+        }
 
-//         for (var i = 0; i < amt; i++) {
-//             families.Add(new Family());
-//         }
+        return new Faker<Customer>()
+            .CustomInstantiator(f =>
+                new Customer(
+                    f.Person.FullName,
+                    f.Internet.Email(),
+                    f.Internet.Password(),
+                    f.Phone.PhoneNumber("##########")
+                )).Generate(count);
+    }
 
-//         context.Families.AddRange(families);
-//         await context.SaveChangesAsync();
+    private static async Task<List<Family>> CreateFamilies(AppDbContext context, int amt)
+    {
+        List<Family> families = [];
 
-//         return families;
-//     }
+        for (var i = 0; i < amt; i++)
+        {
+            families.Add(new Family());
+        }
 
-//     [Fact]
+        context.Families.AddRange(families);
+        await context.SaveChangesAsync();
 
-//     public async Task AddAdmin_ShouldCreateAdminInDatabase()
-//     {
-//         var options = GetDbContextOptions();
+        return families;
+    }
 
-//         var admins = new Faker<Admin>()
-//             .CustomInstantiator(f =>
-//                 new Admin(
-//                     f.Person.FullName,
-//                     f.Internet.Email(),
-//                     f.Internet.Password(),
-//                     f.Phone.PhoneNumber("##########")
-//                 ))
-//             .Generate(5);
+    private static List<Coach> GenerateCoaches(int amt)
+    {
+        return new Faker<Coach>()
+            .CustomInstantiator(f =>
+                new Coach(
+                    f.Person.FullName,
+                    f.Internet.Email(),
+                    f.Internet.Password(),
+                    f.Phone.PhoneNumber("##########")
+                )).Generate(amt);
+    }
 
-//         await using (var context = new AppDbContext(options))
-//         {
-//             await TruncateTablesAsync(context);
+    [Fact]
 
-//             await context.Admins.AddRangeAsync(admins); // Add the generated Admins to the context
-//             await context.SaveChangesAsync(); // Save changes to the database
+    public async Task AddAdmin_ShouldCreateAdminInDatabase()
+    {
+        var options = GetDbContextOptions();
 
-//             var adminCount = await context.Accounts.CountAsync(); // Count the number of Admins
-//             Assert.Equal(5, adminCount); // Ensure there are exactly 5 Admins
-//         }
-//     }
+        var admins = new Faker<Admin>()
+            .CustomInstantiator(f =>
+                new Admin(
+                    f.Person.FullName,
+                    f.Internet.Email(),
+                    f.Internet.Password(),
+                    f.Phone.PhoneNumber("##########")
+                ))
+            .Generate(5);
 
-//     [Fact]
-//     public async Task AddCustomer_ShouldCreateCustomerInDatabase()
-//     {
-//         var options = GetDbContextOptions();
+        await using (var context = new AppDbContext(options))
+        {
+            TruncateTables(context);
 
-//         var customers = GenerateCustomers(5);
+            await context.Admins.AddRangeAsync(admins); // Add the generated Admins to the context
+            await context.SaveChangesAsync(); // Save changes to the database
 
-//         await using (var context = new AppDbContext(options))
-//         {
-//             await TruncateTablesAsync(context);
+            var adminCount = await context.IndividualAccounts.CountAsync(); // Count the number of Admins
+            Assert.Equal(5, adminCount); // Ensure there are exactly 5 Admins
+        }
+    }
 
-//             await context.Customers.AddRangeAsync(customers);
-//             await context.SaveChangesAsync(); // Save changes to the database
+    [Fact]
+    public async Task AddCustomer_ShouldCreateCustomerInDatabase_No_Family()
+    {
+        var options = GetDbContextOptions();
 
-//             var customerCount = await context.Customers.CountAsync(); // Count the number of customers
-//             var accountsCount = await context.Accounts.CountAsync(); // Count the number of customers
+        var customers = GenerateCustomers(5, null, null);
 
-//             Assert.Equal(5, customerCount); // Ensure there are exactly 5 Customers
-//             Assert.Equal(5, accountsCount); // Ensure there are exactly 5 Customers
-//         }
-//     }
+        await using (var context = new AppDbContext(options))
+        {
+            TruncateTables(context);
 
-//     [Fact]
-//     public async Task AddFamily_ShouldCreateFamilyInDatabase()
-//     {
-//         var options = GetDbContextOptions();
+            await context.Customers.AddRangeAsync(customers);
+            await context.SaveChangesAsync(); // Save changes to the database
 
-//         await using (var context = new AppDbContext(options))
-//         {
-//             await TruncateTablesAsync(context);
+            var customerCount = await context.Customers.CountAsync(); // Count the number of customers
+            var accountsCount = await context.IndividualAccounts.CountAsync(); // Count the number of customers
 
-//             await CreateFamilies(context, 5);
+            Assert.Equal(5, customerCount); // Ensure there are exactly 5 Customers
+            Assert.Equal(5, accountsCount); // Ensure there are exactly 5 Customers
+        }
+    }
 
-//             var familiesCount = await context.Families.CountAsync(); // Count the number of families
+    [Fact]
+    public async Task AddFamily_ShouldCreateFamilyInDatabase()
+    {
+        var options = GetDbContextOptions();
 
-//             Assert.Equal(5, familiesCount); // Ensure there are exactly 5 families
-//         }
-//     }
+        await using (var context = new AppDbContext(options))
+        {
+            TruncateTables(context);
 
-//     [Fact]
-//     public async Task AddFamilyMember_ShouldCreateFamilyMemberInDatabase()
-//     {
-//         var options = GetDbContextOptions();
+            await CreateFamilies(context, 5);
 
-//         await using (var context = new AppDbContext(options))
-//         {
-//             await TruncateTablesAsync(context);
+            var familiesCount = await context.Families.CountAsync(); // Count the number of families
 
-//             var customers = GenerateCustomers(20);
+            Assert.Equal(5, familiesCount); // Ensure there are exactly 5 families
+        }
+    }
 
-//             await context.Customers.AddRangeAsync(customers);
+    [Fact]
+    public async Task AddCustomers_With_Family_ShouldCreate_Customers_And_Family_And_Accounts_InDatabase()
+    {
+        var options = GetDbContextOptions();
 
-//             List<Family> families = await CreateFamilies(context, 5);
+        await using (var context = new AppDbContext(options))
+        {
+            TruncateTables(context);
 
-//             List<FamilyMember> members = [];
+            List<Family> families = await CreateFamilies(context, 5);
 
-//             for (int i = 0; i < 7; i++)
-//             {
-//                 var customer = customers[i];
-//                 var randomFamily = families[new Random().Next(families.Count)];
-//                 List<FamilyMember.RolesEnum> roles = [FamilyMember.RolesEnum.Child, FamilyMember.RolesEnum.Parent];
+            List<Customer.RolesEnum> roles = [Customer.RolesEnum.Child, Customer.RolesEnum.Parent];
 
-//                 members.Add(new FamilyMember(randomFamily.Id, customer.Id, roles[new Random().Next(roles.Count)]));
-//             }
+            var customers = GenerateCustomers(20, families, roles);
 
-//             await context.FamilyMembers.AddRangeAsync(members);
+            await context.Customers.AddRangeAsync(customers);
 
-//             await context.SaveChangesAsync();
+            await context.SaveChangesAsync();
 
-//             var accountsCount = await context.Accounts.CountAsync(); // Count the number of customers
+            var accountsCount = await context.Customers.CountAsync(); // Count the number of customers
 
-//             Assert.Equal(20, customers.Count); // Ensure there are exactly 5 Customers
-//             Assert.Equal(20, accountsCount); // Ensure there are exactly 5 Customers
-//             Assert.Equal(5, families.Count);
-//         }
-//     }
+            Assert.Equal(20, customers.Count); // Ensure there are exactly 5 Customers
+            Assert.Equal(20, accountsCount); // Ensure there are exactly 5 Customers
+            Assert.Equal(5, families.Count);
+        }
+    }
+
+    [Fact]
+    public async Task AddCoaches_ShouldCreateCoachesInDatabase()
+    {
+        var options = GetDbContextOptions();
+
+        await using (var context = new AppDbContext(options))
+        {
+            TruncateTables(context);
+
+            var coaches = GenerateCoaches(5);
+
+            await context.Coaches.AddRangeAsync(coaches);
+
+            await context.SaveChangesAsync();
+
+            var coachesCount = await context.Coaches.CountAsync(); // Count the number of coaches
+            var accountsCount = await context.IndividualAccounts.CountAsync(); // Count the number of coaches
+
+            Assert.Equal(5, coachesCount); // Ensure there are exactly 5 coaches
+            Assert.Equal(5, accountsCount); // Ensure there are exactly 5 accounts
+        }
+    }
 
 
-// }
+}
